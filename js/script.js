88088c0a594db66c2b93e82d4946d878f3563c32
Michele Prova <michele.prova99@gmail.com>
2020-07-06 22:35:05 +0200

    fine

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Inserisci il tuo colore preferito');
+document.getElementById('test').innerHTML= (nome + cognome + colore);
